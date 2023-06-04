e3d17334b8967703ed7e01aa45d942761b79d8df
Andrei Dubov <av.dubov@yandex.ru>
2023-06-04 11:42:45 +0400

    made the cache implementation better

--- cache.go
+++ cache.go
@@ -1,52 +1,6 @@
 package cache
 
-import (
-	"sync"
-	"time"
-
-	"github.com/pkg/errors"
-)
-
 type Cache interface {
-	Set(key string, value interface{}, ttl time.Duration)
-	Get(key string) (interface{}, error)
-	Delete(key string)
-}
-
-type cache struct {
-	store map[string]interface{}
-	mutex *sync.RWMutex
-}
-
-func New() Cache {
-	return &cache{
-		store: make(map[string]interface{}),
-		mutex: new(sync.RWMutex),
-	}
-}
-
-func (c *cache) Set(key string, value interface{}, ttl time.Duration) {
-	c.mutex.Lock()
-	c.store[key] = value
-	c.mutex.Unlock()
-
-	go func(ttl time.Duration) {
-		time.Sleep(ttl)
-		c.Delete(key)
-	}(ttl)
-}
-
-func (c *cache) Get(key string) (interface{}, error) {
-	c.mutex.RLock()
-	defer c.mutex.RUnlock()
-	if _, exist := c.store[key]; !exist {
-		return nil, errors.Errorf("there is no item that has the key [%s]", key)
-	}
-	return c.store[key], nil
-}
-
-func (c *cache) Delete(key string) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	delete(c.store, key)
+	Set(key, value interface{}, ttl int64) error
+	Get(key interface{}) (interface{}, error)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/andredubov/cache
 
 go 1.17
-
-require github.com/pkg/errors v0.9.1
